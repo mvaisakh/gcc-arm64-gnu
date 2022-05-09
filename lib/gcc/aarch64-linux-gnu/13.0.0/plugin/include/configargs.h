8e243edef92da8597083accaca64d28dde1b882f
Vaisakh Murali <mvaisakh@statixos.com>
2022-05-09 15:43:14 +0600

    gcc-gnu: Import GCC ARM64 20220509
    Using built-in specs.
    COLLECT_GCC=bin/aarch64-linux-gnu-gcc
    COLLECT_LTO_WRAPPER=/home/mvaisakh/projects/gcc-arm64gnu/bin/../libexec/gcc/aarch64-linux-gnu/13.0.0/lto-wrapper
    Target: aarch64-linux-gnu
    Configured with: ../gcc/configure --target=aarch64-linux-gnu CFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' --prefix=/home/mvaisakh/projects/gcc/../gcc-arm64gnu --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot : (reconfigured) ../gcc/configure --target=aarch64-linux-gnu CFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' --prefix=/home/mvaisakh/projects/gcc/../gcc-arm64gnu --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion='Eva GCC' --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 13.0.0 20220509 (Bleeding Edge) (Eva GCC)
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ lib/gcc/aarch64-linux-gnu/13.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-linux-gnu CFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' --prefix=/home/mvaisakh/projects/gcc/../gcc-arm64gnu --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot : (reconfigured) ../gcc/configure --target=aarch64-linux-gnu CFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections' --prefix=/home/mvaisakh/projects/gcc/../gcc-arm64gnu --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion='Eva GCC' --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
